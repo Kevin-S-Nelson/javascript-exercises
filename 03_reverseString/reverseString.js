73ce8b0e4114dc78c106f1120baecee83bdf1c95
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2022-12-04 12:57:10 -0600

    Create a loop that starts at the last element of the orriginal array and pushes it to the next element in the reversed array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,7 +2,10 @@ const reverseString = function(someString) {
     stringReversed = ""; 
     someArray = someString.split("");
     arrayReversed = [];
-
+    for (let i = someArray.length - 1; i >= 0; i--) {
+        arrayReversed.push(someArray[i]);
+    }
+    return arrayReversed;
 };
 
 // Do not edit below this line
