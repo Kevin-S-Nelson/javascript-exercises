d1399194cdb641ec49cb37d1b605c9fc67a45727
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2022-12-04 12:40:52 -0600

    Add input values to the function, create an if else statement with a for loop embeded that meets all requirements.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(someString, numberOfRepeats) {
+    concatString = "";
+    if (numberOfRepeats >= 0) {
+        for (let i = 0; i < numberOfRepeats; i++) {
+            concatString += someString;
+        }
+    } else {
+        concatString = "ERROR"
+    }    
+    return concatString;
 };
 
 // Do not edit below this line
