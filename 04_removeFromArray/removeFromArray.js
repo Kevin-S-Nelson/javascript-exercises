425f0da4c6d43ddc4e884e31a45203a696abdfd4
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2022-12-05 05:19:55 -0600

    Add comments to explain the 'why'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,19 @@
 const removeFromArray = function(someArray) {
     let newArray = [];
     let match = false;
-    for (let i = 0; i < someArray.length; i++) {
+    for (let i = 0; i < someArray.length; i++) { 
         for (let j = 1; j < arguments.length; j++) {
             if (someArray[i] === arguments[j]) {
                 match = true;
-                break;
+                break; // Match found, no need to keep checking
             } else {
                 match = false;
             }
         }
         if (match) {
-            continue;
+            continue; // If it matches, we do not want to add the new array
         } else {
-            newArray.push(someArray[i]);
+            newArray.push(someArray[i]); // If it does have a match, add it
         }
     }
     return newArray;
