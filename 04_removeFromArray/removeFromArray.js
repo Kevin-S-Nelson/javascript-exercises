ee2e09ad50a9ed16832d81c11ee83c40faddd59b
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2022-12-05 05:09:05 -0600

    Create function that passes first two tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,35 @@
-const removeFromArray = function() {
+/*const removeFromArray = function(someArray) {
+    let newArray = [];
+    let j = 1;
+    for (let i = 0; i < someArray.length; i++) {
+        if (someArray[i] == arguments[j]) {
+            newArray = newArray;
+        } else {
+            newArray.push(someArray[i]);
+        }
+    }
+    return newArray;
+};*/
 
+const removeFromArray = function(someArray) {
+    let newArray = [];
+    let match = false;
+    for (let i = 0; i < someArray.length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (someArray[i] == arguments[j]) {
+                match = true;
+                break;
+            } else {
+                match = false;
+            }
+        }
+        if (match) {
+            continue;
+        } else {
+            newArray.push(someArray[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
