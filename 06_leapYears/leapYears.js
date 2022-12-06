f77d500e86672e8a9bbc7a712c07b559aa71af31
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2022-12-05 19:51:48 -0600

    Add a test and logic for if the number is divisable by 100, and then 400

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,13 @@
 const leapYears = function(year) {
     let isLeapYear = false;
-    if (year % 4 === 0) {
+    if (year % 4 === 0 && year % 100 !== 0) {
         isLeapYear = true;
+    } else if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            isLeapYear = true;
+        } else {
+            isLeapYear = false;
+        }
     } else {
         isLeapYear = false;
     }
