8d4aac950a500b7dec418c415ee3f93c3061c2ea
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2023-04-14 04:39:59 -0500

    add functionality to palindrome code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (someStr) {
+    let isPalindrome = false;
+    // first translate the sting to all lower case
+    let lowerStr = someStr.toLowerCase();
+    // then ignore anything that isn't a lower case letter (space, special character)
+    let cleanStr = lowerStr.replace(/[^a-z]/g, '');
+    // create a new sting that is the reverse of the first sting
+    function reverseString(str) {
+        let newStr = "";
+        for (let i = str.length - 1; i >= 0; i--) {
+            newStr += str[i];
+        }
+        return newStr;
+    } 
+        // console.log(reverseString(cleanStr));
+        // console.log(cleanStr);
+    // compare the two strings and if they match return true
+    if (reverseString(cleanStr) == cleanStr) {
+        isPalindrome = true;
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
