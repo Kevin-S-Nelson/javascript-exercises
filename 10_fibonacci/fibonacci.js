f59bb1c8952e58b02db03ea9ae3629a3ce29de25
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2023-04-14 05:00:28 -0500

    add fib functionality

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibIndex) {
+    let fibArray = [1,1];
+    if (fibIndex < 0) {
+        return "OOPS";
+    }
+    for (let i = 2; i < fibIndex; i++) {
+        fibArray.push(fibArray[i-2] + fibArray[i-1]);
+    }
+    return fibArray[fibIndex - 1];
 };
 
 // Do not edit below this line
