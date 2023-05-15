564cf60620116aa47e50b09b392f035b3ea521ff
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2023-05-15 05:39:46 -0500

    Add functionality to complete the first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPersonIndex = 0;
+    // Calculate the age of each person and the largest number for age
+    for (let i = 0; i < people.length; i++) {
+        people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
+        if (people[i].age > oldestAge) {
+            oldestAge = people[i].age;
+            oldestPersonIndex = i;
+        }
+    }
+return people[oldestPersonIndex];
 };
 
 // Do not edit below this line
