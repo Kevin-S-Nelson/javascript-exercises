33ae3e36cd07a6a6839a6c09b62e12964e7dc826
kevin-s-nelson <47140767+Kevin-S-Nelson@users.noreply.github.com>
2023-06-05 04:58:09 -0500

    Add statement that checks if a person is still living

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,6 +3,10 @@ const findTheOldest = function(people) {
     let oldestPersonIndex = 0;
     // Calculate the age of each person and the largest number for age
     for (let i = 0; i < people.length; i++) {
+        // Check if current person is still living
+        if (typeof people[i].yearOfDeath == "undefined") {
+            people[i].yearOfDeath = new Date().getFullYear();
+        }
         people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
         if (people[i].age > oldestAge) {
             oldestAge = people[i].age;
